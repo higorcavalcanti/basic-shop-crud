78719e03d5fe9a9d17cbd4dcc1e39d0d92fd4297
Higor Cavalcanti <higordaniell@live.com>
2021-08-16 20:25:12 -0300

    Fixed main routing

--- Front/src/app/containers/main/main-routing.module.ts
+++ Front/src/app/containers/main/main-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
       {
         path: '',
         pathMatch: 'full',
-        redirectTo: 'order'
+        redirectTo: 'orders'
       },
       {
         path: 'clients',
