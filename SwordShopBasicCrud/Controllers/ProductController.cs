030c66b1f6c63d0bf58d3754f4837246168d4a91
Higor Cavalcanti <higordaniell@live.com>
2021-08-17 10:06:13 -0300

    Product Controller

---
+++ SwordShopBasicCrud/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SwordShopBasicCrud.Models;
+using SwordShopBasicCrud.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwordShopBasicCrud.Controllers
+{
+    public class ProductController : BaseController<Product>
+    {
+        public ProductController(BaseService<Product> service) : base(service)
+        {
+
+        }
+    }
+}
